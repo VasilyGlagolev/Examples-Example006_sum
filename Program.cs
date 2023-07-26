683b223bd7ce4794bc472303f8c28e3c18bc08c3
VasilyGlagolev <139496040+VasilyGlagolev@users.noreply.github.com>
2023-07-26 22:28:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Число чётное");
+        }
+        else
+        {
+            Console.WriteLine("Число нечётное");
+        }
+
+        Console.ReadKey();
+    }
+}
+ 
